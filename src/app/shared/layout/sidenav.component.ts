9a13b9fb762c706f142c459def3b83a528b1e0c2
Ricardo Rojas <reddrum7@gmail.com>
2018-10-30 22:39:35 -0500

    [Fix] Fix detect breakpoints using Angular CDK

--- src/app/shared/layout/sidenav.component.ts
+++ src/app/shared/layout/sidenav.component.ts
@@ -1,26 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
 
-const smallWidthBreakPoint = 720;
-
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-  private mediaMatcher: MediaQueryList =
-    matchMedia(`(max-width: ${smallWidthBreakPoint}px)`);
   isScreenSmall: boolean;
-
 
   constructor(public breakpointObserver: BreakpointObserver) {}
 
   ngOnInit() {
     this.breakpointObserver
-      .observe([Breakpoints.Small])
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
       .subscribe((state: BreakpointState) => {
-          this.isScreenSmall = state.matches;
+        this.isScreenSmall = state.matches ? true : false;
     });
   }
 
