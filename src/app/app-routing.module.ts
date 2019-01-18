25bee0f329a6d5075403ca763e5f835668f1e1fa
Ricardo Rojas <reddrum7@gmail.com>
2019-01-17 21:26:02 -0500

    [Refactor] Reorder routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,24 @@ import { AboutComponent } from './components/about/about.component';
 import { ArticleListComponent } from './articles/components/article-list/article-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'articles', pathMatch: 'full' },
-  { path: 'about', component: AboutComponent },
-  { path: 'articles', component: ArticleListComponent },
-  { path: '**', redirectTo: '' }
+  {
+    path: 'articles',
+    component: ArticleListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    redirectTo: '/articles',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/articles',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
